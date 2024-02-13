559c4c77d25ed36148b7bfe60ee3e4346ef16abc
grapix_1060 <anthony.kalaydjian@outlook.com>
2024-02-13 18:38:17 +0100

    work on progress

---
+++ src/k_means_attention/my_attention_simulator.py
@@ -0,0 +1,371 @@
+import os
+import time
+import random
+import math 
+import datetime as dt
+from typing import Union
+
+from tqdm import tqdm
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch import Tensor
+from torch.nn import BatchNorm1d, Identity
+from torch.nn import Linear
+from torch_geometric.loader import DataLoader
+from torch_geometric.data import Data
+
+from lips import get_root_path
+from lips.benchmark.airfransBenchmark import AirfRANSBenchmark
+from lips.dataset.airfransDataSet import download_data
+from lips.dataset.scaler.standard_scaler_iterative import StandardScalerIterative
+
+
+# K-means clustering
+
+# using a uniform at random initialisation
+def centroid_uniform_initialisation(X:Tensor, k: int=100):
+    samples = np.random.choice(a=X.shape[0], replace=False, size=k)
+    return X[samples,:]
+
+# using the k-means++ initialisation
+def kmeans_plusplus_initialisation(X: torch.Tensor, k: int):
+    n_samples = X.shape[0]
+    # Step 1: Choose one center uniformly at random from among the data points.
+    centroids = X[torch.randint(0, n_samples, (1,))]
+
+    for _ in range(k - 1):
+        # Step 2: For each data point x, compute D(x), the distance between x and the nearest center that has already been chosen.
+        distances = torch.cdist(X, centroids, p=2)
+        min_distances = torch.min(distances, dim=1)[0]
+
+        # Step 3: Choose one new data point at random as a new center, using a weighted probability distribution where a point x is chosen with probability proportional to D(x)^2.
+        probabilities = min_distances / torch.sum(min_distances)
+        new_centroid_idx = torch.multinomial(probabilities, 1)
+
+        # Add the new centroid to the list of centroids
+        centroids = torch.cat([centroids, X[new_centroid_idx]], dim=0)
+
+    return centroids
+
+def k_means(X:Tensor, k: int=6):
+    centroids = kmeans_plusplus_initialisation(X,k)
+
+    while True:
+        # compute the distance of each point to the centroids
+        distances = torch.cdist(X, centroids, p=2)
+
+        # define the index associated to each centroid
+        cluster_idx = torch.argmin(distances, dim=1)
+
+        # compute the new centroids
+        new_centroids = torch.stack([X[cluster_idx == i].mean(0) for i in range(k)])
+
+        # end when we have converged
+        if torch.all(centroids == new_centroids):
+            break
+
+        centroids = new_centroids
+    return cluster_idx, centroids
+
+
+def smoothL1(pred, target, keptcomponent=False):
+    x = pred - target
+    y = torch.minimum(x.abs(), x * x)
+    if keptcomponent:
+        return y.mean(0)
+    else:
+        return y.mean()
+
+
+def smoothSoftmax(x):
+    s = torch.nn.functional.softmax(x, dim=1)
+    ss = torch.nn.functional.relu(x)
+    num = ss * 0.1 + s
+    denom = num.sum(1).unsqueeze(-1)
+    return num / denom
+
+
+class AttentionBlock(torch.nn.Module):
+    def __init__(self, sIN, sOUT, sPROJ=None):
+        super(AttentionBlock, self).__init__()
+
+        if sPROJ is None:
+            sPROJ = sOUT
+
+        self.k = torch.nn.Linear(sIN, sPROJ)
+        self.q = torch.nn.Linear(sIN, sPROJ)
+        self.v = torch.nn.Linear(sIN, sPROJ)
+
+    def forward(self, x: Tensor, y: Tensor) -> Tensor:
+        YK = self.k(y)
+        XQ = self.q(x)
+        YV = self.v(y)
+
+        M = torch.matmul(XQ, YK.t())
+        M = smoothSoftmax(M)
+        output = torch.matmul(M, YV)
+
+        return output
+
+
+class Ransformer(torch.nn.Module):
+    def __init__(self):
+        super(Ransformer, self).__init__()
+
+        self.proj1 = AttentionBlock(7, 32)
+        self.proj2 = AttentionBlock(39, 121)
+        self.proj3 = AttentionBlock(128, 249)
+        self.proj4 = AttentionBlock(256, 256)
+        self.proj5 = torch.nn.Linear(256, 4)
+
+    def forward(self, x, y):
+        z = self.proj1(x, y)
+
+        z = torch.cat([z, x], dim=1)
+        z = self.proj2(z, y)
+
+        z = torch.cat([z, x], dim=1)
+        z = self.proj3(z, y)
+
+        z = torch.cat([z, x], dim=1)
+        z = self.proj4(z, y)
+        
+        z = self.proj5(z)
+        return z
+
+
+class AugmentedSimulator():
+    def __init__(self,benchmark,**kwargs):
+        self.name = "AirfRANSSubmission"
+        chunk_sizes=benchmark.train_dataset.get_simulations_sizes()
+        scalerParams={"chunk_sizes":chunk_sizes}
+        self.scaler = StandardScalerIterative(**scalerParams)
+
+        self.model = None
+        self.hparams = kwargs
+        use_cuda = torch.cuda.is_available()
+        self.device = 'cuda:0' if use_cuda else 'cpu'
+        if use_cuda:
+            print('Using GPU')
+        else:
+            print('Using CPU')
+
+        self.model = Ransformer(**self.hparams)
+
+    def process_dataset(self, dataset, training: bool) -> DataLoader:
+        coord_x=dataset.data['x-position']
+        coord_y=dataset.data['y-position']
+        surf_bool=dataset.extra_data['surface']
+        position = np.stack([coord_x,coord_y],axis=1)
+
+        nodes_features,node_labels=dataset.extract_data()
+        if training:
+            print("Normalize train data")
+            nodes_features, node_labels = self.scaler.fit_transform(nodes_features, node_labels)
+            print("Transform done")
+        else:
+            print("Normalize not train data")
+            nodes_features, node_labels = self.scaler.transform(nodes_features, node_labels)
+            print("Transform done")
+
+        torchDataset=[]
+        nb_nodes_in_simulations = dataset.get_simulations_sizes()
+        start_index = 0
+        # check alive
+        t = dt.datetime.now()
+
+        for nb_nodes_in_simulation in nb_nodes_in_simulations:
+            #still alive?
+            if dt.datetime.now() - t > dt.timedelta(seconds=60):
+                print("Still alive - index : ", end_index)
+                t = dt.datetime.now()
+            end_index = start_index+nb_nodes_in_simulation
+            simulation_positions = torch.tensor(position[start_index:end_index,:], dtype = torch.float) 
+            simulation_features = torch.tensor(nodes_features[start_index:end_index,:], dtype = torch.float) 
+            simulation_labels = torch.tensor(node_labels[start_index:end_index,:], dtype = torch.float) 
+            simulation_surface = torch.tensor(surf_bool[start_index:end_index])
+
+            sampleData=Data(pos=simulation_positions,
+                            x=simulation_features, 
+                            y=simulation_labels,
+                            surf = simulation_surface.bool()) 
+            torchDataset.append(sampleData)
+            start_index += nb_nodes_in_simulation
+        
+        return DataLoader(dataset=torchDataset,batch_size=self.hparams["batch_size"])
+
+    def train(self,train_dataset, save_path=None):
+        train_dataset = self.process_dataset(dataset=train_dataset,training=True)
+        print("Start training")
+        model = global_train(self.device, train_dataset, self.model, self.hparams,criterion = 'L1Smooth')
+        print("Training done")
+
+    def predict(self,dataset,**kwargs):
+        print(dataset)
+        test_dataset = self.process_dataset(dataset=dataset,training=False)
+        self.model.eval()
+        avg_loss_per_var = np.zeros(4)
+        avg_loss = 0
+        avg_loss_surf_var = np.zeros(4)
+        avg_loss_vol_var = np.zeros(4)
+        avg_loss_surf = 0
+        avg_loss_vol = 0
+        iterNum = 0
+
+        predictions=[]
+        with torch.no_grad():
+            for data in test_dataset:        
+                data_clone = data.clone()
+                data_clone = data_clone.to(self.device)
+                out = self.model(data_clone.x)
+
+                targets = data_clone.y
+                loss_criterion = nn.MSELoss(reduction = 'none')
+
+                loss_per_var = loss_criterion(out, targets).mean(dim = 0)
+                loss = loss_per_var.mean()
+                loss_surf_var = loss_criterion(out[data_clone.surf, :], targets[data_clone.surf, :]).mean(dim = 0)
+                loss_vol_var = loss_criterion(out[~data_clone.surf, :], targets[~data_clone.surf, :]).mean(dim = 0)
+                loss_surf = loss_surf_var.mean()
+                loss_vol = loss_vol_var.mean()  
+
+                avg_loss_per_var += loss_per_var.cpu().numpy()
+                avg_loss += loss.cpu().numpy()
+                avg_loss_surf_var += loss_surf_var.cpu().numpy()
+                avg_loss_vol_var += loss_vol_var.cpu().numpy()
+                avg_loss_surf += loss_surf.cpu().numpy()
+                avg_loss_vol += loss_vol.cpu().numpy()  
+                iterNum += 1
+
+                out = out.cpu().data.numpy()
+                prediction = self._post_process(out)
+                predictions.append(prediction)
+        print("Results for test")
+        print(avg_loss/iterNum, avg_loss_per_var/iterNum, avg_loss_surf_var/iterNum, avg_loss_vol_var/iterNum, avg_loss_surf/iterNum, avg_loss_vol/iterNum)
+        predictions= np.vstack(predictions)
+        predictions = dataset.reconstruct_output(predictions)
+        return predictions
+
+    def _post_process(self, data):
+        try:
+            processed = self.scaler.inverse_transform(data)
+        except TypeError:
+            processed = self.scaler.inverse_transform(data.cpu())
+        return processed
+
+def global_train(device, train_dataset, network, hparams, criterion = 'L1Smooth', reg = 1):
+    model = network.to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr = hparams['lr'])
+    lr_scheduler = torch.optim.lr_scheduler.OneCycleLR(
+            optimizer,
+            max_lr = hparams['lr'],
+            total_steps = (len(train_dataset) // hparams['batch_size'] + 1) * hparams['nb_epochs'],
+        )
+    start = time.time()
+
+    train_loss_surf_list = []
+    train_loss_vol_list = []
+    loss_surf_var_list = []
+    loss_vol_var_list = []
+
+    pbar_train = tqdm(range(hparams['nb_epochs']), position=0)
+    epoch_nb = 0
+
+    for epoch in pbar_train:
+        epoch_nb += 1
+        print('Epoch: ', epoch_nb)
+        train_dataset_sampled = []
+        for data in train_dataset:
+            data_sampled = data.clone()
+            idx = random.sample(range(data_sampled.x.size(0)), hparams['subsampling'])
+            idx = torch.tensor(idx)
+
+            data_sampled.pos = data_sampled.pos[idx]
+            data_sampled.x = data_sampled.x[idx]
+            data_sampled.y = data_sampled.y[idx]
+            data_sampled.surf = data_sampled.surf[idx]
+            train_dataset_sampled.append(data_sampled)
+        train_loader = DataLoader(train_dataset_sampled, batch_size = hparams['batch_size'], shuffle = True)
+        del(train_dataset_sampled)
+
+        train_loss, _, loss_surf_var, loss_vol_var, loss_surf, loss_vol = train_model(device, model, train_loader, optimizer, lr_scheduler, criterion, reg = reg)        
+        if criterion == 'MSE_weighted':
+            train_loss = reg*loss_surf + loss_vol
+        del(train_loader)
+
+        train_loss_surf_list.append(loss_surf)
+        train_loss_vol_list.append(loss_vol)
+        loss_surf_var_list.append(loss_surf_var)
+        loss_vol_var_list.append(loss_vol_var)
+
+    loss_surf_var_list = np.array(loss_surf_var_list)
+    loss_vol_var_list = np.array(loss_vol_var_list)
+
+    return model
+
+def train_model(device, model, train_loader, optimizer, scheduler, criterion='L1Smooth', reg: Union[int, None]=1.0):
+    model.train()
+    avg_loss_per_var = torch.zeros(4, device = device)
+    avg_loss = 0
+    avg_loss_surf_var = torch.zeros(4, device = device)
+    avg_loss_vol_var = torch.zeros(4, device = device)
+    avg_loss_surf = 0
+    avg_loss_vol = 0
+    iterNum = 0
+    
+    for data in train_loader:
+        data_clone = data.clone()
+        data_clone = data_clone.to(device)   
+        optimizer.zero_grad()
+
+        # extracting a skeleton from the cloudpoint
+        _, centroids = k_means(data.pos, k=model.hparams['k'])
+
+        clustroid_idx = []
+        for point in centroids:
+            clustroid_idx.append(torch.argmin(((data_clone.pos - point)**2).mean(dim=1)).item())
+
+        skeleton = torch.clone(data["x"][clustroid_idx,:])
+
+        out = model(data_clone.x, skeleton)
+        targets = data_clone.y
+
+        loss_criterion = nn.MSELoss(reduction = 'none')
+        if criterion == 'MSE':
+            loss_criterion = nn.MSELoss(reduction = 'none')
+        elif criterion == 'MAE':
+            loss_criterion = nn.L1Loss(reduction = 'none')
+        elif criterion == 'L1Smooth':
+            loss_criterion = smoothL1
+        
+        loss_per_var = loss_criterion(out, targets).mean(dim = 0)
+        total_loss = loss_per_var.mean()
+        loss_surf_var = loss_criterion(out[data_clone.surf, :], targets[data_clone.surf, :]).mean(dim = 0)
+        loss_vol_var = loss_criterion(out[~data_clone.surf, :], targets[~data_clone.surf, :]).mean(dim = 0)
+        loss_surf = loss_surf_var.mean()
+        loss_vol = loss_vol_var.mean()
+
+        if (reg is not None):            
+            (loss_vol + reg*loss_surf).backward()           
+        else:
+            total_loss.backward()
+        
+        optimizer.step()
+        scheduler.step()
+        avg_loss_per_var += loss_per_var
+        avg_loss += total_loss
+        avg_loss_surf_var += loss_surf_var
+        avg_loss_vol_var += loss_vol_var
+        avg_loss_surf += loss_surf
+        avg_loss_vol += loss_vol 
+        iterNum += 1
+
+    return  avg_loss.cpu().data.numpy()/iterNum,            \
+            avg_loss_per_var.cpu().data.numpy()/iterNum,    \
+            avg_loss_surf_var.cpu().data.numpy()/iterNum,   \
+            avg_loss_vol_var.cpu().data.numpy()/iterNum,    \
+            avg_loss_surf.cpu().data.numpy()/iterNum,       \
+            avg_loss_vol.cpu().data.numpy()/iterNum
