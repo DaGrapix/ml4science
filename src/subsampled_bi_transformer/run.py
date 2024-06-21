6e98ca62ef5c9d8952351385434aabbfa7ff8068
Grapix-pc <anthony.kalaydjian@outlook.com>
2024-06-21 12:56:28 +0200

    fixed typo

--- src/subsampled_bi_transformer/run.py
+++ src/subsampled_bi_transformer/run.py
@@ -1,7 +1,7 @@
 from ingestion import run_model
 
 root_dir = r"C:/Users/antho/Desktop/ml4physim/src/run/"                 # Add the path to the run directory of the project here
-model_name = "bi-transformer"                                           # Use the name of the model's directory name, eg. bi_transformer
+model_name = "bi_transformer"                                           # Use the name of the model's directory name, eg. bi_transformer
 model_path = "models/" + model_name
 BENCHMARK_PATH = r"C:/Users/antho/Desktop/ml4physim/src/benchmark.pkl"  # Add the path to the pickled benchmark file here. If the file does not exist, it will be created there.
 
